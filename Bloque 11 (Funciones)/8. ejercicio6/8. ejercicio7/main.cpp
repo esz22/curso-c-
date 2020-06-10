9a6dac8eaa5e0be03970945173c10d36dddddcae
Enrique Soto <esz22@hotmail.com>
2020-06-09 21:55:56 -0500

    ejercicio 6 del bloque de funciones

--- Bloque 11 (Funciones)/8. ejercicio6/8. ejercicio7/main.cpp
+++ Bloque 11 (Funciones)/8. ejercicio6/8. ejercicio7/main.cpp
@@ -1,9 +1,38 @@
+/*escriba una plantilla de funcion llamada maximo() que devuelva el valor maximo de tres argumentos
+que se transmitan a la funcion cuando sea llamada. Suponga que los tres argumentos seran del mismo tipo de datos.
+*/
 #include <iostream>
+#include <stdlib.h>
 
 using namespace std;
 
+template <class tipod>
+tipod maximo(tipod x, tipod y, tipod z);
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    int a=0;
+    int b=0;
+    int c=0;
+
+    cout<<"el valor maximo es: "<<maximo(a,b,c)<<endl;
+
+    system("pause");
     return 0;
+}
+template <class tipod>
+tipod maximo(tipod x, tipod y, tipod z){
+    tipod maxim;
+    if(y>x && y>z){
+        maxim=y;
+    }
+    else if(x>y && x>z){
+        maxim=x;
+    }
+    else if(z>y && z>x){
+        maxim=z;
+    }
+    else{
+        maxim=false;
+    }
 }
